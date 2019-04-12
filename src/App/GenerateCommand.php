52aa0c0a62a72011435cb1471dcc31eba946ce42
= <blaize.kaye@gmail.com>
2019-04-12 15:16:05 +1200

    Reduces size for api limit

--- src/App/GenerateCommand.php
+++ src/App/GenerateCommand.php
@@ -14,7 +14,7 @@ class GenerateCommand extends Command
 
     const NUMBER_OF_USERS = 20;
 
-    const NUMBER_OF_ISSUES = 40;
+    const NUMBER_OF_ISSUES = 10;
 
     protected function configure()
     {
